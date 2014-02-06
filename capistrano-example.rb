7d0d32423b52183c10e416d062c36e5c136d9e2c
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-02-06 15:18:52 +0100

    Do not use sudo in deployment

--- capistrano-example.rb
+++ capistrano-example.rb
@@ -6,6 +6,8 @@ set :scm, :git
 
 set :db_pwd, 'db4kune'
 
+set :use_sudo, false
+
 role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 role :app, "your app-server here"                          # This may be the same as your `Web` server
 role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
